3b0b3d07cc48e3e51e51f4f9243e40ce4b1f8de7
carla-moutou <carla.moutou@isen.yncrea.fr>
2021-02-07 20:44:59 +0100

    first commit

---
+++ app/src/main/java/fr/isen/moutou/AndroidErestaurant/CategoryActivity.kt
@@ -0,0 +1,74 @@
+package fr.isen.moutou.AndroidErestaurant
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.restoagnes.databinding.ActivityCategoryBinding
+import com.example.restoagnes.models.Dish
+import com.example.restoagnes.models.Menu_result
+import com.google.gson.GsonBuilder
+import org.json.JSONObject
+
+
+enum class ItemTypes{ENTREES, PLATS, DESSERTS}
+class CategoryActivity {
+
+
+    class CategoryActivity : AppCompatActivity() {
+
+        private lateinit var binding:ActivityCategoryBinding//lateinit pour l'initialiser plus tard
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding= ActivityCategoryBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+            val selectedItem = intent.getSerializableExtra(HomeActivity.CATEGORY_NAME) as? ItemTypes
+            binding.categorytitle.text=getcategorytitle(selectedItem)
+
+            launchrequest()
+        }
+
+        private fun getcategorytitle(item: ItemTypes?):String{
+            return when(item){
+                ItemTypes.ENTREES->"Entrées"
+                ItemTypes.PLATS->"Plats"
+                ItemTypes.DESSERTS->"Desserts"
+
+                else -> ""
+            }
+        }
+
+        private fun launchrequest(){
+            val q = Volley.newRequestQueue(this)
+            val url = "http://test.api.catering.bluecodegames.com/menu"
+            val jsondata = JSONObject()
+            jsondata.put("id_shop",1)//ajout clé valeur
+            val request = JsonObjectRequest(Request.Method.POST,
+                url,
+                jsondata,
+                {
+                    val menuResult = GsonBuilder().create().fromJson(it.toString(),Menu_result::class.java)
+                    //recuperer menu result en objet
+                    loadlist(menuResult.data.first().items)
+                },//response listener
+                {
+                    Log.d("Request",it.toString())
+                })//reponse error listener
+            q.add(request)
+        }
+
+        private fun loadlist(list:List<Dish>){
+            val adapteur = CategoryAdapter(list) { dish ->
+                Log.d("dish", "Selected dish ${dish.name}")
+
+            }
+            binding.recyclerView.layoutManager= LinearLayoutManager(this)//affichage en liste
+            binding.recyclerView.adapter = adapteur
+        }
+    }
+}
---
+++ app/src/main/java/fr/isen/moutou/AndroidErestaurant/HomeActivity.kt
@@ -0,0 +1,41 @@
+package fr.isen.moutou.AndroidErestaurant
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonEntrees.setOnClickListener{
+            val intent = Intent(this,CategoryActivity::class.java)
+            intent.putExtra(CATEGORY_NAME,ItemTypes.ENTREES)
+            startActivity(intent)
+        }
+
+        binding.buttonPlats.setOnClickListener{
+            val intent = Intent(this,CategoryActivity::class.java)
+            intent.putExtra(CATEGORY_NAME,ItemTypes.PLATS)
+            startActivity(intent)
+        }
+
+        binding.buttonDesserts.setOnClickListener{
+            val intent = Intent(this,CategoryActivity::class.java)
+            intent.putExtra(CATEGORY_NAME,ItemTypes.DESSERTS)
+            startActivity(intent)
+        }
+    }
+    //put extra (clé valeur)
+    companion object{
+        const val CATEGORY_NAME = "category name"
+    }
+
+}
